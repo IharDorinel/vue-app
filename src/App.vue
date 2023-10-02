dae6fd2c206440ba76055bc4ee0cd71025f57bba
IharDorinel <ihardaronin@gmail.com>
2023-10-02 13:39:14 +0300

    create vue game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <MonstersGame/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MonstersGame from './components/MonstersGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MonstersGame
   }
 }
 </script>
